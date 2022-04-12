6b2f504cf08521d2a9a556d5a4e14ad7a45ac097
Kemirun <eladiyaolukemi@gmail.com>
2022-04-12 16:42:38 -0700

    0x02 Functions & Nested Loops

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -6,11 +6,8 @@
 
 void print_alphabet(void)
 {
-	char ch;
-
-	for (ch = 'a'; ch <= 'z'; ch++)
-		
-		_putchar(ch);
-
-	_putchar('\n');
+char ch;
+for (ch = 'a'; ch <= 'z'; ch++)
+_putchar(ch)
+_putchar('\n');
 }
